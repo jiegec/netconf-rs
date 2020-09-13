95f7888f9fc440d9a2919c05a7b9637e1385ae04
Jiajie Chen <c@jia.je>
2020-09-13 19:02:11 +0800

    Require Transport to be Send

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -5,7 +5,7 @@ use std::io;
 pub mod ssh;
 
 /// Trait for NETCONF transport
-pub trait Transport {
+pub trait Transport: Send {
     fn read_xml(&mut self) -> io::Result<String>;
     fn write_xml(&mut self, data: &str) -> io::Result<()>;
 }
